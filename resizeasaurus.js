149d81629e2b82e31cd8edbc1b718cebc0ddc773
Zach Leatherman <zachleatherman@gmail.com>
2023-10-04 09:00:37 -0500

    Too many adopted stylesheets! Better cut the mustard

--- resizeasaurus.js
+++ resizeasaurus.js
@@ -1,6 +1,8 @@
-if( "customElements" in window) {
-	class ResizeASaurus extends HTMLElement {
-		static css = `
+
+class ResizeASaurus extends HTMLElement {
+	static needsCss = true;
+
+	static css = `
 @supports (resize: horizontal) {
 	resize-asaurus:not([disabled]):defined {
 		display: grid;
@@ -29,58 +31,63 @@ if( "customElements" in window) {
 	}
 }
 `;
-		constructor() {
-			super();
+	constructor() {
+		super();
 
-			this.attrs = {
-				label: "label"
-			};
+		this.attrs = {
+			label: "label"
+		};
 
-			this.classes = {
-				sizer: "resizeasaurus-size"
-			};
+		this.classes = {
+			sizer: "resizeasaurus-size"
+		};
+	}
 
+	connectedCallback() {
+		// https://caniuse.com/mdn-api_cssstylesheet_replacesync
+		if(!CSS.supports("resize", "horizontal") || !("replaceSync" in CSSStyleSheet.prototype) || this.hasAttribute("disabled")) {
+			return;
+		}
+
+		if(ResizeASaurus.needsCss) {
 			let sheet = new CSSStyleSheet();
 			sheet.replaceSync(ResizeASaurus.css);
 			document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+			ResizeASaurus.needsCss = false;
 		}
 
-		connectedCallback() {
-			if(!CSS.supports("resize", "horizontal") || this.hasAttribute("disabled")) {
-				return;
-			}
-
-			if(this.getAttribute(this.attrs.label) === "disabled") {
-				return;
-			}
-
-			this.size = this.querySelector(`.${this.classes.sizer}`);
-			if(!this.size) {
-				this.size = document.createElement("output");
-				this.size.style.display = "none"; // in case css isn’t available.
-				this.size.classList.add(this.classes.sizer);
-				this.size.textContent = "Drag to resize";
-				this.appendChild(this.size);
-			}
-
-			if("ResizeObserver" in window) {
-				let isSet = false;
-				this.resizer = new ResizeObserver(entries => {
-					let width = this.clientWidth + "px";
-					this.size.innerHTML = `${parseInt(width, 10) / 16}em (${width})`;
-					if(!window.safari && !isSet) {
-						isSet = true;
-						this.style.setProperty("--resizeasaurus-initial-width", width);
-					}
-				});
-				this.resizer.observe(this);
-			}
+		if(this.getAttribute(this.attrs.label) === "disabled") {
+			return;
 		}
 
-		resize() {
-			this.size.innerHTML = this.outerWidth;
+		this.size = this.querySelector(`.${this.classes.sizer}`);
+		if(!this.size) {
+			this.size = document.createElement("output");
+			this.size.style.display = "none"; // in case css isn’t available.
+			this.size.classList.add(this.classes.sizer);
+			this.size.textContent = "Drag to resize";
+			this.appendChild(this.size);
+		}
+
+		if("ResizeObserver" in window) {
+			let isSet = false;
+			this.resizer = new ResizeObserver(entries => {
+				let width = this.clientWidth + "px";
+				this.size.innerHTML = `${parseInt(width, 10) / 16}em (${width})`;
+				if(!window.safari && !isSet) {
+					isSet = true;
+					this.style.setProperty("--resizeasaurus-initial-width", width);
+				}
+			});
+			this.resizer.observe(this);
 		}
 	}
 
+	resize() {
+		this.size.innerHTML = this.outerWidth;
+	}
+}
+
+if("customElements" in window) {
 	customElements.define("resize-asaurus", ResizeASaurus);
 }
